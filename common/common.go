06fe000451460481c47f515a29073a25c919fae0
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-10-16 18:15:48 +0200

    Add expression with invalid operator o

--- common/common.go
+++ common/common.go
@@ -9,9 +9,10 @@ const OperatorsList = "+ - * / ^ sqrt"
 
 // Two examples and their expected results
 const (
-	RPNInput     = "6 8 4.0 sqrt + 3.01 1.99 + - *" //= 30.000000
-	Input        = "3 4 2 * 1 5 - 2 3 ^ ^ / +"      //= 3.0001220703125
-	InvalidInput = "3 4 2 * ? 1 5 - 2 3 ^ ^ / +"    // No value
+	RPNInput        = "6 8 4.0 sqrt + 3.01 1.99 + - *" //= 30.000000
+	Input           = "3 4 2 * 1 5 - 2 3 ^ ^ / +"      //= 3.0001220703125
+	InvalidInput    = "3 4 2 * ? 1 5 - 2 3 ^ ^ / +"    // No value
+	InvalidOperator = "3 4 2 * o 1 5 - 2 3 ^ ^ / +"    // No value
 
 	RPNInputWant = 30.000000
 	InputWant    = 3.0001220703125
