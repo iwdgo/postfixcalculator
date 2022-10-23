3587cd08fb54bf1dac289ca48f5977547639e624
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-10-23 15:52:04 +0200

    Add one operand band test which should be satisfied

--- turingmachine/rpnturingmachine_test.go
+++ turingmachine/rpnturingmachine_test.go
@@ -22,6 +22,14 @@ func TestRPNTuringMachine(t *testing.T) {
 	}
 }
 
+func TestRPNTuringMachine_oneoperand(t *testing.T) {
+	t.Skip("One operand band should return the value")
+	i := 1.0
+	if got := RPNTuringMachine("1"); got != i {
+		t.Fatalf("got %v, want %v", got, i)
+	}
+}
+
 func TestRPNTuringMachine_panicOperator(t *testing.T) {
 	t.Skip("Panics on operand error and not unknown operator")
 	defer func() {
