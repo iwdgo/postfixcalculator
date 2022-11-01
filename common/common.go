2e050cbd0243b641ad783d648d0c4b775e9e78a7
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-11-01 10:35:29 +0100

    Common: add elementary values

--- common/common.go
+++ common/common.go
@@ -7,7 +7,6 @@ import (
 
 const OperatorsList = "+ - * / ^ sqrt"
 
-// Two examples and their expected results
 const (
 	RPNInput            = "6 8 4.0 sqrt + 3.01 1.99 + - *" //= 30.000000
 	Input               = "3 4 2 * 1 5 - 2 3 ^ ^ / +"      //= 3.0001220703125
@@ -15,6 +14,8 @@ const (
 	InvalidLeftOperand  = "a 4 *"                          // a is not a valid operand
 	InvalidRightOperand = "3 4,3 *"                        // 4,3 is not a valid operand
 	InvalidOperator     = "3 4 o"                          // o is not a known operator
+	OneOperand          = "1"
+	OneOperator         = "+"
 
 	RPNInputWant = 30.000000
 	InputWant    = 3.0001220703125
