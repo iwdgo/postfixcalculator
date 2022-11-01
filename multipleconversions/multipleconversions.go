cd61be8f470d348e4e9991b0e1e8e1e7290512d1
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-11-01 10:12:31 +0100

    Multiple conversions: update comments
    - inline operations to align on other implementations

--- multipleconversions/multipleconversions.go
+++ multipleconversions/multipleconversions.go
@@ -17,53 +17,53 @@ func RPN(RPNInput string) float64 {
 	var num, num2 float64
 	var err error
 	for len(words) != 1 {
-		//index = findFirstOperator(words)
-		//Skipping subroutine
 		err = nil
 		index = 0
 		for err == nil && index < len(words) {
-			_, err = strconv.ParseFloat(words[index], 64) //0 iso 64 returns an int for some reason
+			_, err = strconv.ParseFloat(words[index], 64)
 			index++
 		}
+		// TODO err is ignored expecting an operator but could be an operand
 		index -= 1
 		if words[index] == "sqrt" {
-			//unary operator
+			// Unary operator
 			if num, err = strconv.ParseFloat(words[index-1], 64); err != nil {
+				// TODO Unreachable
 				panic("Invalid value for sqrt")
 			}
-			// num = math.Sqrt(num)
 			words[index-1] = strconv.FormatFloat(math.Sqrt(num), 'f', 10, 64)
-			words = append(words[:index], words[index+1:]...) //removing sqrt
+			// Remove operator sqrt
+			words = append(words[:index], words[index+1:]...)
 		} else {
-			//binary operator
+			// Binary operator
 			if num, err = strconv.ParseFloat(words[index-2], 64); err != nil {
 				panic("Invalid left operand")
 			}
 			if num2, err = strconv.ParseFloat(words[index-1], 64); err != nil {
 				panic("Invalid right operand")
 			}
-			words[index-2] = returnStringResult(num, num2, words[index])
-			/* removing binary operator by removing the slice until the item after the result */
+			switch words[index] {
+			case "+":
+				words[index-2] = strconv.FormatFloat(num+num2, 'f', 13, 64)
+			case "-":
+				words[index-2] = strconv.FormatFloat(num-num2, 'f', 13, 64)
+			case "*":
+				words[index-2] = strconv.FormatFloat(num*num2, 'f', 13, 64)
+			case "/":
+				words[index-2] = strconv.FormatFloat(num/num2, 'f', 13, 64)
+			case "^":
+				words[index-2] = strconv.FormatFloat(math.Pow(num, num2), 'f', 13, 64)
+			default:
+				// TODO Unreachable
+				panic("Invalid operator")
+			}
+			// Remove operator and right operand
 			words = append(words[:index-1], words[index+1:]...)
 		}
 	}
-	num, _ = strconv.ParseFloat(words[0], 64)
-	return num
-}
-
-func returnStringResult(leftOp, rightOp float64, operatorU string) string {
-	switch operatorU {
-	case "+":
-		return strconv.FormatFloat(leftOp+rightOp, 'f', 13, 64)
-	case "-":
-		return strconv.FormatFloat(leftOp-rightOp, 'f', 13, 64)
-	case "*":
-		return strconv.FormatFloat(leftOp*rightOp, 'f', 13, 64)
-	case "/":
-		return strconv.FormatFloat(leftOp/rightOp, 'f', 13, 64)
-	case "^":
-		return strconv.FormatFloat(math.Pow(leftOp, rightOp), 'f', 13, 64)
-	default:
-		panic("Invalid operator")
+	num, err = strconv.ParseFloat(words[0], 64)
+	if err != nil {
+		panic("Invalid operator or operand")
 	}
+	return num
 }
