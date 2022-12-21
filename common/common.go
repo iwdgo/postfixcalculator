ae72f206d57be0321adcfb1c68088f1f6384d93b
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-12-21 09:29:22 +0100

    Remove constants which are only used once

--- common/common.go
+++ common/common.go
@@ -8,13 +8,9 @@ import (
 const OperatorsList = "+ - * / ^ sqrt"
 
 const (
-	RPNInput            = "6 8 4.0 sqrt + 3.01 1.99 + - *" //= 30.000000
-	Input               = "3 4 2 * 1 5 - 2 3 ^ ^ / +"      //= 3.0001220703125
-	InvalidInput        = "3 4 2 * ? 1 5 - 2 3 ^ ^ / +"    // No value
-	InvalidLeftOperand  = "a 4 *"                          // a is not a valid operand
-	InvalidRightOperand = "3 4,3 *"                        // 4,3 is not a valid operand
-	InvalidOperator     = "3 4 o"                          // o is not a known operator
-	OneOperator         = "+"
+	RPNInput     = "6 8 4.0 sqrt + 3.01 1.99 + - *" //= 30.000000
+	Input        = "3 4 2 * 1 5 - 2 3 ^ ^ / +"      //= 3.0001220703125
+	InvalidInput = "3 4 2 * ? 1 5 - 2 3 ^ ^ / +"    // No value
 
 	RPNInputWant = 30.000000
 	InputWant    = 3.0001220703125
@@ -22,13 +18,14 @@ const (
 
 type Rpn func(s string) float64
 
+// Common tests
 func PanicRightOperand(t *testing.T, RPN Rpn) {
 	defer func() {
 		if r := recover(); r == nil {
 			t.Fatal("The code did not panic")
 		}
 	}()
-	_ = RPN(InvalidRightOperand)
+	_ = RPN("3 4,3 *") // 4,3 is not a valid right operand
 }
 
 func PanicLeftOperand(t *testing.T, RPN Rpn) {
@@ -37,7 +34,7 @@ func PanicLeftOperand(t *testing.T, RPN Rpn) {
 			t.Fatal("The code did not panic")
 		}
 	}()
-	_ = RPN(InvalidLeftOperand)
+	_ = RPN("a 4 *") // a is not a valid left operand
 }
 
 func OneInvalidOperator(t *testing.T, RPN Rpn) {
@@ -63,5 +60,5 @@ func PanicOperator(t *testing.T, RPN Rpn) {
 			t.Fatal("The code did not panic")
 		}
 	}()
-	_ = RPN(InvalidOperator)
+	_ = RPN("3 4 o") // o is not a known operator
 }
