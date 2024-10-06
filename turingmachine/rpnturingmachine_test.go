86b1f6baa6d20dbbfd44ad8b0ebb16fccb6f7e20
Constantin Konstantinidis <constantinkonstantinidis@gmail.com>
2024-10-06 19:36:17 +0100

    Turing machine: run all tests as skip instructions are obsolete

--- turingmachine/rpnturingmachine_test.go
+++ turingmachine/rpnturingmachine_test.go
@@ -6,23 +6,19 @@ import (
 )
 
 func TestOneOperand(t *testing.T) {
-	t.Skip("one operand is incorrectly handled")
 	common.OneOperand(t, RPNTuringMachine)
 }
 
 func TestPanicOperator(t *testing.T) {
-	t.Skip("Panics on operand error and not unknown operator")
 	common.PanicOperator(t, RPNTuringMachine)
 }
 
 func TestNoOperator(t *testing.T) {
-	t.Skip("No operator returns 0")
 	s := "1 2 3"
 	_ = RPNTuringMachine(s)
 }
 
 func TestOneInvalidOperator(t *testing.T) {
-	t.Skip("does not panic but returns 0")
 	common.OneInvalidOperator(t, RPNTuringMachine)
 }
 
